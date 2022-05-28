d32e9a187c252b8db04a0de93d4dc9613ed983ee
Mayur Garg <mayur7garg@gmail.com>
2022-05-29 04:37:15 +0530

    Remove redundant copies to bound mounts in dockerfiles

--- db/Setup_DB.sh
+++ db/Setup_DB.sh
@@ -1,6 +1,6 @@
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i Db_Init.sql
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i DB_Init.sql
     if [ $? -eq 0 ]
     then
         echo "'ExpenseManager' DB creation successful!"
